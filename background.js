af1703b81c0e5663f6f44f56a0370189cd8fd4b8
Alexey Mironov <alexmir2015@yandex.ru>
2022-09-08 12:35:22 +0300

    change listener

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
-const utmRegexp = /([?&])utm_[A-z]+=[A-z0-9_]+(&|$)/g
+const utmRegexp = /(\?|\&)(utm_[^=]+\=[^&]*)/g
 
-chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-    console.log('details.url', details.url)
-    if(details.url.indexOf("www.google.com") !== -1) {
-        chrome.tabs.update(details.tabId, {
-            url: details.url.replace(utmRegexp, '')
-        });
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+       if(utmRegexp.test(details.url)) {
+           chrome.tabs.update(details.tabId, {url: details.url.replace(utmRegexp, '').replace('&', '?')});
+        }
+    },
+    {
+        urls: ["<all_urls>"],
+        types: ["main_frame"]
     }
-});
+);
